f4cda4adf264ad2a0fe5fdfed30268dadb9fe6f1
Wasin Thonkaew <haxpor@gmail.com>
2017-06-17 06:41:55 +0800

    Add more tilesets

---
+++ android/assets/tileset/spawn-tilesets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spawn-tiles" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="spawn-tiles.png" width="64" height="32"/>
+</tileset>
