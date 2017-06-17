033eab0b426396ab9dbc484b6cf44e8719b7b03f
Wasin Thonkaew <haxpor@gmail.com>
2017-06-18 05:58:32 +0800

    Fix z-order for entities that needed to be sorted first before drawing

---
+++ core/src/io/wasin/raceplant/entities/SortSprite.kt
@@ -0,0 +1,11 @@
+package io.wasin.raceplant.entities
+
+import com.badlogic.gdx.graphics.g2d.Sprite
+
+/**
+ * Created by haxpor on 6/18/17.
+ */
+class SortSprite(sprite: Sprite, zOrder: Float) {
+    var zOrder: Float = zOrder
+    var sprite: Sprite = sprite
+}
