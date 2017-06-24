3718a068b65150c43f49366cc8487a72457a7c7b
Wasin Thonkaew <haxpor@gmail.com>
2017-06-24 18:15:34 +0800

    Add animated water tile

--- android/assets/tileset/all-tilesets.tsx
+++ android/assets/tileset/all-tilesets.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="all-tilesets" tilewidth="32" tileheight="32" spacing="2" tilecount="225" columns="15">
- <image source="all-tilesets.png" width="512" height="512"/>
+<tileset name="all-tilesets" tilewidth="32" tileheight="32" spacing="2" tilecount="45" columns="15">
+ <image source="all-tilesets.png" width="512" height="128"/>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="600"/>
+   <frame tileid="11" duration="600"/>
+  </animation>
+ </tile>
 </tileset>
